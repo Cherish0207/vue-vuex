be6b188da2fc958c23a4e15855eb6377b97a63eb
Cherish0207 <cheshire_1207@foxmail.com>
2021-01-30 15:17:27 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    珠峰的年齡: {{ $store.state.age }} <br />
+    我的年龄: {{ $store.getters.getAge }} <br />
+    <button @click="$store.commit('changeAge', 5)">同步更新状态</button>
+    <button @click="$store.dispatch('changeAge', 5)">异步更新状态</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    age: 10,
+  },
+  getters: {
+    getAge(state) {
+      return state.age + 18;
+    },
+  },
+  // vue中的方法唯一可以改状态方法,同步的
+  mutations: {
+    changeAge(state, payload) {
+      state.age += payload;
+    },
+  },
+  actions: {
+    changeAge({ commit }, payload) {
+      setTimeout(() => {
+        commit("changeAge", payload);
+      }, 2000);
+    },
+  },
+  modules: {},
+});
